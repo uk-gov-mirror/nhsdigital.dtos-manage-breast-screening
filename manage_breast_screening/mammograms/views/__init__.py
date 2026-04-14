d5d82b62d62e2991bbf626d556637d451c4efa50
Graham Pengelly <graham@three-tier.com>
2026-04-14 17:29:05 +0100

    Implement gateway_images_enabled flag
    * Replace the existing env var based flag with a shiny new openfeature
      one
    
    This is a basic test implementation that will apply accross all
    environments currently

--- manage_breast_screening/mammograms/views/__init__.py
+++ manage_breast_screening/mammograms/views/__init__.py
@@ -1,10 +1,9 @@
-import os
-
+from manage_breast_screening.core.feature_flags import FeatureFlag
 from manage_breast_screening.gateway.models import Relay
 
 
 def gateway_images_enabled(appointment):
     """Check if automatic gateway image retrieval is enabled."""
-    if os.getenv("GATEWAY_IMAGES_ENABLED", "false").lower() == "true":
+    if FeatureFlag.is_enabled("gateway_images"):
         return Relay.for_appointment(appointment) is not None
     return False
