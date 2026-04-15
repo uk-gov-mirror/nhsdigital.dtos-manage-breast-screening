5e33584b4e60c0dcb717fe990a0902c1a28f77b3
Steve Laing <steve.laing@gmail.com>
2026-04-15 17:30:20 +0100

    Add a Managed Identity token validator
    Uses key lookups for azure. See https://github.com/Azure-Samples/ms-identity-python-webapi-azurefunctions/blob/master/Function/secureFlaskApp/__init__.py

---
+++ manage_breast_screening/dicom/tests/test_token_validator.py
@@ -0,0 +1,121 @@
+import json
+from unittest.mock import Mock, patch
+
+import jwt
+import pytest
+
+from ..token_validator import TokenValidator
+
+
+@patch(f"{TokenValidator.__module__}.logger")
+class TestTokenValidator:
+    @pytest.fixture
+    def mock_urlopen_response(self):
+        return {
+            "keys": [
+                {
+                    "kid": "abc123",
+                    "kty": "RSA",
+                    "use": "sig",
+                    "n": "modulus",
+                    "e": "exponent",
+                }
+            ]
+        }
+
+    @pytest.fixture
+    def mock_urlopen(self, mock_urlopen_response):
+        with patch(f"{TokenValidator.__module__}.urlopen") as mock_urlopen:
+            mock_urlopen.return_value.read.return_value = json.dumps(
+                mock_urlopen_response
+            ).encode("utf-8")
+            yield mock_urlopen
+
+    @pytest.fixture
+    def mock_get_unverified_header(self):
+        with patch(
+            f"{TokenValidator.__module__}.jwt.get_unverified_header"
+        ) as mock_get_unverified_header:
+            mock_get_unverified_header.return_value = {"kid": "abc123"}
+            yield mock_get_unverified_header
+
+    @patch(f"{TokenValidator.__module__}.urlopen")
+    def test_with_no_matching_rsa_key(self, mock_urlopen, mock_logger):
+        mock_urlopen.return_value.read.return_value = '{"keys": []}'
+        validator = TokenValidator()
+        assert validator(Mock(headers={"Authorization": "Bearer abc123"})) is None
+        mock_logger.error.assert_called_with("Unable to find appropriate key")
+
+    def test_with_matching_rsa_key(self, _, mock_get_unverified_header, mock_urlopen):
+        validator = TokenValidator()
+        rsa_key = validator._rsa_key("abc123")
+        assert rsa_key == {
+            "kty": "RSA",
+            "kid": "abc123",
+            "use": "sig",
+            "n": "modulus",
+            "e": "exponent",
+        }
+
+    @patch(
+        f"{TokenValidator.__module__}.jwt.decode", side_effect=jwt.ExpiredSignatureError
+    )
+    def test_with_expired_signature(
+        self, mock_jwt_decode, mock_logger, mock_get_unverified_header, mock_urlopen
+    ):
+        validator = TokenValidator()
+        assert validator(Mock(headers={"Authorization": "Bearer abc123"})) is None
+        mock_logger.error.assert_called_with("Token is expired", exc_info=True)
+
+    @patch(
+        f"{TokenValidator.__module__}.jwt.decode", side_effect=jwt.InvalidAudienceError
+    )
+    def test_with_invalid_claims(
+        self, mock_jwt_decode, mock_logger, mock_get_unverified_header, mock_urlopen
+    ):
+        validator = TokenValidator()
+        assert validator(Mock(headers={"Authorization": "Bearer abc123"})) is None
+        mock_logger.error.assert_called_with(
+            "Invalid claims. Please check the audience and issuer.", exc_info=True
+        )
+
+    @patch(
+        f"{TokenValidator.__module__}.jwt.decode", side_effect=jwt.InvalidIssuerError
+    )
+    def test_with_invalid_issuer(
+        self, mock_jwt_decode, mock_logger, mock_get_unverified_header, mock_urlopen
+    ):
+        validator = TokenValidator()
+        assert validator(Mock(headers={"Authorization": "Bearer abc123"})) is None
+        mock_logger.error.assert_called_with(
+            "Invalid claims. Please check the audience and issuer.", exc_info=True
+        )
+
+    @patch(f"{TokenValidator.__module__}.jwt.decode", side_effect=jwt.InvalidTokenError)
+    def test_with_invalid_token(
+        self, mock_jwt_decode, mock_logger, mock_get_unverified_header, mock_urlopen
+    ):
+        validator = TokenValidator()
+        assert validator(Mock(headers={"Authorization": "Bearer abc123"})) is None
+        mock_logger.error.assert_called_with("Token is invalid", exc_info=True)
+
+    @patch(f"{TokenValidator.__module__}.jwt.decode", side_effect=Exception)
+    def test_with_unexpected_exception(
+        self, mock_jwt_decode, mock_logger, mock_get_unverified_header, mock_urlopen
+    ):
+        validator = TokenValidator()
+        assert validator(Mock(headers={"Authorization": "Bearer abc123"})) is None
+        mock_logger.error.assert_called_with(
+            "Unable to parse authentication token.", exc_info=True
+        )
+
+    @patch(
+        f"{TokenValidator.__module__}.jwt.decode", return_value={"sub": "1234567890"}
+    )
+    def test_with_valid_token(
+        self, mock_jwt_decode, _, mock_get_unverified_header, mock_urlopen
+    ):
+        validator = TokenValidator()
+        assert validator(Mock(headers={"Authorization": "Bearer abc123"})) == {
+            "sub": "1234567890"
+        }
---
+++ manage_breast_screening/dicom/token_validator.py
@@ -0,0 +1,68 @@
+import json
+import logging
+import os
+from urllib.request import urlopen
+
+import jwt
+from ninja.security import HttpBearer
+
+logger = logging.getLogger(__name__)
+
+ALLOWED_ALGORITHMS = ["RS256"]
+
+
+class TokenValidator(HttpBearer):
+    def __init__(self):
+        self.api_audience = os.getenv("API_AUDIENCE", "")
+        self.tenant_id = os.getenv("TENANT_ID", "")
+        self.discovery_keys_url = (
+            "https://login.microsoftonline.com/"
+            + self.tenant_id
+            + "/discovery/v2.0/keys"
+        )
+        self.issuer_url = "https://sts.windows.net/" + self.tenant_id + "/"
+
+    def authenticate(self, request, token) -> dict | None:
+        rsa_key = self._rsa_key(token)
+        if rsa_key:
+            return self._decode(token, rsa_key)
+
+        logger.error("Unable to find appropriate key")
+
+    def _rsa_key(self, token) -> dict | None:
+        try:
+            jsonurl = urlopen(self.discovery_keys_url)
+            jwks = json.loads(jsonurl.read())
+            unverified_header = jwt.get_unverified_header(token)
+            for key in jwks["keys"]:
+                if key["kid"] == unverified_header["kid"]:
+                    return {
+                        "kty": key["kty"],
+                        "kid": key["kid"],
+                        "use": key["use"],
+                        "n": key["n"],
+                        "e": key["e"],
+                    }
+        except Exception:
+            logger.error("Unable to parse authentication token.", exc_info=True)
+
+    def _decode(self, token: str, rsa_key: dict) -> dict | None:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALLOWED_ALGORITHMS,
+                audience=self.api_audience,
+                issuer=self.issuer_url,
+            )
+            return payload
+        except jwt.ExpiredSignatureError:
+            logger.error("Token is expired", exc_info=True)
+        except (jwt.InvalidAudienceError, jwt.InvalidIssuerError):
+            logger.error(
+                "Invalid claims. Please check the audience and issuer.", exc_info=True
+            )
+        except jwt.InvalidTokenError:
+            logger.error("Token is invalid", exc_info=True)
+        except Exception:
+            logger.error("Unable to parse authentication token.", exc_info=True)
