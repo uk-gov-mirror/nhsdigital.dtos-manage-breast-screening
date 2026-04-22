faed8a11e8dbeb5f3b7339a4930b242bd558c5c9
Steve Webber <198724792+swebberuk@users.noreply.github.com>
2026-04-22 16:49:56 +0100

    What is your opinion of these images? page

--- manage_breast_screening/reading/urls.py
+++ manage_breast_screening/reading/urls.py
@@ -5,5 +5,10 @@ from . import views
 app_name = "reading"
 
 urlpatterns = [
-    path("", views.show_reading_dashboard, name="show_reading_dashboard"),
+    path("", views.show_reading_dashboard_view, name="show_reading_dashboard"),
+    path(
+        "sessions/<uuid:session_pk>/reads/<uuid:pk>/",
+        views.ReadImageView.as_view(),
+        name="image_read",
+    ),
 ]
