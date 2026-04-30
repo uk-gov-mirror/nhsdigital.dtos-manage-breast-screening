9ffc3d907c814ab28028fad0d6fdb57b5be451e3
Steve Laing <steve.laing@gmail.com>
2026-04-30 14:40:17 +0100

    Add Gateway to admin models
    Allows us to create Gateway instances and associate them with a Relay.

--- manage_breast_screening/gateway/admin.py
+++ manage_breast_screening/gateway/admin.py
@@ -2,10 +2,26 @@ from django.contrib import admin
 
 from manage_breast_screening.core.admin import admin_site
 
-from .models import Relay
+from .models import Gateway, Relay
+
+
+@admin.register(Gateway, site=admin_site)
+class GatewayAdmin(admin.ModelAdmin):
+    list_display = ["name", "oid", "resource_name"]
+    search_fields = ["name", "oid", "resource_name"]
+    list_filter = ["created_at", "updated_at"]
+    ordering = ["name"]
 
 
 @admin.register(Relay, site=admin_site)
 class RelayAdmin(admin.ModelAdmin):
-    list_display = ["setting", "namespace", "hybrid_connection_name"]
-    list_select_related = ["setting"]
+    list_display = ["setting", "gateway", "namespace", "hybrid_connection_name"]
+    list_select_related = ["setting", "gateway"]
+    search_fields = [
+        "setting__name",
+        "gateway__name",
+        "namespace",
+        "hybrid_connection_name",
+    ]
+    list_filter = ["created_at", "updated_at"]
+    ordering = ["setting__name", "gateway__name", "namespace", "hybrid_connection_name"]
